913f40ae2f4256ea259e6f9d46b2f377786fe054
Ed-Marcavage <edmarcavage256@proton.me>
2022-09-22 17:00:30 -0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChainId, DAppProvider } from "@usedapp/core";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const config = {
+  readOnlyChainId: ChainId.Goerli,
+  readOnlyUrls: {
+    [ChainId.Goerli]:
+      "https://goerli.infura.io/v3/61fd3f1e41834c5ebd42c6d193267d74",
+  },
+  notifications: {
+    expirationPeriod: 1000, //milliseconds
+    checkInterval: 1000, // milliseconds
+  },
+};
 root.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>
 );
 
